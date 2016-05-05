3f07211e42adcb5785b06ef05c1c7edd54397487
Walker Flieller <wflieller@gmail.com>
2016-05-05 12:43:08 -0500

    Added Search form and Contact form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,13 +3,24 @@ class StaticPagesController < ApplicationController
   end
 
    def landing_page
-    # @featured_product = Product.first
     @products = Product.limit(3)
   end
 
   def about
   end
 
+
+
   def contact
   end
+
+	def thank_you
+	  @name = params[:name]
+	  @email = params[:email]
+	  @message = params[:message]
+	  ActionMailer::Base.mail(:from => @email,
+	      :to => 'admin@disruptlabs.io',
+	      :subject => "A new contact form message from #{@name}",
+	      :body => @message).deliver_now
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
+  post 'static_pages/thank_you'
+
   root 'static_pages#landing_page'
 
 
